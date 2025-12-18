ee6149a9623d5ca46e433aea84aa9a7c44971465
lechibang-1512 <176822289+lechibang-1512@users.noreply.github.com>
2025-12-18 13:46:04 +0000

    bigbangcommit:moved language specific api to a different repo. note to create a gitmodules later

--- tools/python_api/test/test_version.py
+++
@@ -1,6 +0,0 @@
-def test_version() -> None:
-    import gorgonzola
-
-    assert gorgonzola.version != ""
-    assert gorgonzola.storage_version > 0
-    assert gorgonzola.version == gorgonzola.__version__
